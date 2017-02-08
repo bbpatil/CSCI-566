566a28f19f8c674c61979a4760d40b2ed4e8c01d
Nate Woods <big.nate.w@gmail.com>
2017-02-07 17:23:39 -0700

    Adding First question solution to code block

---
+++ QuestionA.cc
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+class Txc : public cSimpleModule {
+  protected:
+    virtual void forwardMessage(cMessage *msg);
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+Define_Module(Txc);
+
+void Txc::initialize() {
+    if (getIndex() == 0) {
+        char msgname[20];
+        sprintf(msgname, "tic-%d", getIndex());
+        cMessage *msg = new cMessage(msgname);
+        scheduleAt(0.0, msg);
+    }
+}
+
+void Txc::handleMessage(cMessage *msg) {
+    if (getIndex() == 3) {
+        EV << "Message " << msg << " arrived.\n";
+        delete msg;
+    } else {
+        forwardMessage(msg);
+    }
+}
+
+void Txc::forwardMessage(cMessage *msg) {
+    int n = gateSize("port");
+
+    // START: Exercise 10 (Question 3) If randomly generated number is same as input gate, generate again
+    cGate *arrival = msg->getArrivalGate();
+    int k;
+    do {
+        k = intuniform(0, n-1);
+    } while (arrival != nullptr && n > 1 && k == arrival->getIndex());
+    // STOP: Exercise 10 (Question 3)
+
+    EV << "Forwarding message " << msg << " on port out[" << k << "]\n";
+    send(msg, "port$o", k);
+}
