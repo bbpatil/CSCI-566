99735ed6697cbf979b3a2d41f7869dc78be83f71
Nate Woods <big.nate.w@gmail.com>
2017-02-09 19:11:05 -0700

    Removing VirtualHttpServer

--- QuestionB.cc
+++ QuestionB.cc
@@ -3,22 +3,47 @@
 #include <omnetpp.h>
 #include "inet/common/ModuleAccess.h"
 #include "inet/applications/httptools/server/HttpServer.h"
+#include "inet/applications/httptools/browser/HttpBrowser.h"
 #include "inet/applications/httptools/common/HttpMessages_m.h"
 #include "inet/transportlayer/contract/tcp/TCPSocket.h"
 #include "inet/transportlayer/contract/tcp/TCPSocketMap.h"
 
+/*
+ *
+ * DONT FORGET TO MAKE ALL THE INET THINGS VIRTUAL!!!
+ *
+ * - HttpServer needs `virtual HttpServerBase`
+ *
+ */
+
 using namespace omnetpp;
 
+//class CDNBrowser : public inet::httptools::HttpBrowser {
+//public:
+//    void sendThisBitch(inet::httptools::HttpRequestMessage msg);
+//};
+//
+//void CDNBrowser::sendThisBitch(inet::httptools::HttpRequestMessage msg) {
+//    inet::httptools::HttpBrowser::sendRequestToServer(msg);
+//}
+
+
 class CDNServerBase : public virtual inet::httptools::HttpServerBase {
+//    public:
+//        CDNServerBase();
+
     protected:
         inet::httptools::HttpReplyMessage* handleGetRequest(inet::httptools::HttpRequestMessage *request, std::string resource);
         cPacket *handleReceivedMessage(cMessage *msg);
 
+
 };
 
-class VirtualHttpServer : public virtual inet::httptools::HttpServer {};
+//CDNServerBase::CDNServerBase() {
+//
+//}
 
-class CDNServer : public virtual VirtualHttpServer, public virtual CDNServerBase {
+class CDNServer : public virtual inet::httptools::HttpServer, public virtual CDNServerBase {
 protected:
     virtual void socketDataArrived(int connId, void *yourPtr, cPacket *msg, bool urgent) override;
 };
