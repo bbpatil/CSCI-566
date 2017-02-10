89f1b708cc054f4293c2fdc34eb63fd1ecd98c0a
Nate Woods <big.nate.w@gmail.com>
2017-02-09 18:29:49 -0700

    ACCEPTING REQUESTS!!!!!

--- QuestionB.cc
+++ QuestionB.cc
@@ -4,23 +4,58 @@
 #include "inet/common/ModuleAccess.h"
 #include "inet/applications/httptools/server/HttpServer.h"
 #include "inet/applications/httptools/common/HttpMessages_m.h"
+#include "inet/transportlayer/contract/tcp/TCPSocket.h"
+#include "inet/transportlayer/contract/tcp/TCPSocketMap.h"
 
 using namespace omnetpp;
 
 class CDNServerBase : public virtual inet::httptools::HttpServerBase {
     protected:
         inet::httptools::HttpReplyMessage* handleGetRequest(inet::httptools::HttpRequestMessage *request, std::string resource);
+        cPacket *handleReceivedMessage(cMessage *msg);
+
 };
 
 class VirtualHttpServer : public virtual inet::httptools::HttpServer {};
 
 class CDNServer : public virtual VirtualHttpServer, public virtual CDNServerBase {
 protected:
-    virtual void handleMessage(cMessage *msg) override;
+    virtual void socketDataArrived(int connId, void *yourPtr, cPacket *msg, bool urgent) override;
 };
 
-void CDNServer::handleMessage(cMessage *msg) {
-    inet::httptools::HttpServer::handleMessage(msg);
+// Direct copy from HttpServer
+void CDNServer::socketDataArrived(int connId, void *yourPtr, cPacket *msg, bool urgent) {
+    EV << "NATE 2.0!!!" << endl;
+    if (yourPtr == nullptr) {
+        EV_ERROR << "Socket establish failure. Null pointer" << endl;
+        return;
+    }
+    inet::TCPSocket *socket = (inet::TCPSocket *)yourPtr;
+
+    // Should be a HttpReplyMessage
+    EV_DEBUG << "Socket data arrived on connection " << connId << ". Message=" << msg->getName() << ", kind=" << msg->getKind() << endl;
+
+    // call the message handler to process the message.
+    cMessage *reply = handleReceivedMessage(msg);
+    if (reply != nullptr) {
+        socket->send(reply);    // Send to socket if the reply is non-zero.
+    }
+    delete msg;    // Delete the received message here. Must not be deleted in the handler!
+}
+
+cPacket *CDNServerBase::handleReceivedMessage(cMessage *msg) {
+    EV << "LISA!!!!" << endl;
+//    return inet::httptools::HttpServerBase::handleReceivedMessage(msg);
+    inet::httptools::HttpRequestMessage *request = check_and_cast<inet::httptools::HttpRequestMessage *>(msg);
+    // ASSUME EVERYTHING FUCKING WORKS!!!
+
+    cStringTokenizer tokenizer = cStringTokenizer(request->heading(), " ");
+    std::vector<std::string> res = tokenizer.asVector();
+
+    EV << "LISA 2.0!!!! " << res[1] << endl;
+    htmlDocsServed++;
+
+    return generateErrorReply(request, 404);
 }
 
 inet::httptools::HttpReplyMessage* CDNServerBase::handleGetRequest(inet::httptools::HttpRequestMessage *request, std::string resource)
